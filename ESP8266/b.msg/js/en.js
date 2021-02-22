e9d322250b6879d3ce0680ba6755d816edbd9d1a
fchart <51936358+fchart@users.noreply.github.com>
2021-02-22 09:55:57 +0800

    Add files via upload

--- ESP8266/b.msg/js/en.js
+++ ESP8266/b.msg/js/en.js
@@ -433,4 +433,4 @@ Blockly.Msg["PROCEDURES_HUE"] = "290";
 Blockly.Msg["COLOUR_HUE"] = "20";
 Blockly.Msg["VARIABLES_DYNAMIC_HUE"] = "310";
 
-Blockly.Msg.CONTROLS_FOR_INPUT_ORDER = "ascending loop";
+Blockly.Msg.CONTROLS_FOR_INPUT_ORDER = "order by";
