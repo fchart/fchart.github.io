6f3b1c56e31a853b5a195dbe7bb564d03f5c6e62
fchart <51936358+fchart@users.noreply.github.com>
2026-09-18 10:14:42 +0800

    Add files via upload

---
+++ fchart/sw.js
@@ -0,0 +1,24 @@
+const CACHE='fchart-pwa-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  if(req.method!=='GET')return;
+  const url=new URL(req.url);
+  if(url.origin!==self.location.origin)return;
+  e.respondWith(
+    caches.match(req,{ignoreSearch:true}).then(cached=>{
+      if(cached)return cached;
+      return fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});
+        return res;
+      }).catch(()=>caches.match('./index.html'));
+    })
+  );
+});
